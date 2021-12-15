b28b4daa6e66806d4a0e81986d97f0428fb3c159
Marcin Bernecki <berneckimarcin.priv@gmail.com>
2021-12-15 12:15:37 +0100

    Animation Update

--- animations/src/App.js
+++ animations/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
-import {Transition} from "react-transition-group";
+import { Transition } from "react-transition-group";
 
 class App extends Component {
   state = {
@@ -31,21 +31,22 @@ class App extends Component {
           Toggle
         </button>
         <br />
-        <Transition in={this.state.showBlock} timeout={300} mountOnEnter>
-            { state => <div
-                style={{
-                    backgroundColor: "red",
-                    width: 100,
-                    height: 100,
-                    margin: "auto",
-                    transition: 'opacity 1s ease-out',
-                    opacity: state === 'exited' ? 0 : 1,
-                }}
-            /> }
-
+        <Transition in={this.state.showBlock} timeout={500} mountOnEnter unmountOnExit>
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                marginLeft: state === 'exiting' ? 0 : '95%',
+                transition: "all 0.5s ease-in",
+                opacity: state === "entering" ? 0 : 1,
+              }}
+            />
+          )}
         </Transition>
         <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        <Backdrop />
+        {this.state.modalIsOpen ? <Backdrop /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
