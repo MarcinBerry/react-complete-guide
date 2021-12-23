278140269f8c28ec1f4801b404791eb0e240595a
Marcin Bernecki <71261155+MarcinBerry@users.noreply.github.com>
2021-12-23 09:05:32 +0100

    Update AllQuotes.js

--- router-project/src/pages/AllQuotes.js
+++ router-project/src/pages/AllQuotes.js
@@ -1,13 +1,31 @@
 import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "Learning React is Fun" },
-  { id: "q2", author: "Maxim", text: "Learning React is Great" },
-  { id: "q3", author: "Ma", text: "Learning React Fun" },
-  { id: "q4", author: "Maxaw", text: "Learning is Fun" },
-];
+const AllQuotes = () => {
+  const {sendRequest, status, data: loadedQuotes, error } = useHttp(
+    getAllQuotes,
+    true
+  );
+useEffect(() => {
+  sendRequest();
+  ), [sendRequest]);
+  
+if)status === 'pending') {
+  return (
+    <div className='centered'>
+      <LoadingSpinner />
+    </div> 
+    );
 
-const AllQuotes = (props) => {
+  if(error) {
+    return <p className='centered focused'>{error}</p>; 
+  }
+  if(status === 'completed' && (!loadingQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />
+      
   return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
